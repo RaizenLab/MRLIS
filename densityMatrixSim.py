79248689408192a393573907fc4271a416986d53
HR-Chance <hchance@utexas.edu>
2026-03-10 14:59:25 -0500

    added run_single_iso, updated ivp solver to be Radau to better handle off resonant excitation and switched from 6 complex ODEs to 9 real ODEs using complex decomp.

--- densityMatrixSim.py
+++ densityMatrixSim.py
@@ -15,14 +15,22 @@ def master_equations(t, y, params):
         params (dict): Dictionary of physical constants.
     """
     
-    # 1. Unpack State Vector
-    # y[0..2] are real populations, y[3..5] are complex coherences
+    # # 1. Unpack State Vector
+    # # y[0..2] are real populations, y[3..5] are complex coherences
+    # rho_gg = y[0]
+    # rho_aa = y[1]
+    # rho_bb = y[2]
+    # rho_ag = y[3]
+    # rho_bg = y[4]
+    # rho_ba = y[5]
     rho_gg = y[0]
     rho_aa = y[1]
     rho_bb = y[2]
-    rho_ag = y[3]
-    rho_bg = y[4]
-    rho_ba = y[5]
+    
+    # Reconstruct complex coherences
+    rho_ag = y[3] + 1j * y[4]
+    rho_bg = y[5] + 1j * y[6]
+    rho_ba = y[7] + 1j * y[8]
 
     # 2. Unpack Parameters
     rabi_1 = params['rabi_1']
@@ -88,21 +96,44 @@ def master_equations(t, y, params):
         -1j*rabi_1*rho_bg
     )
 
-    return [d_rho_gg, d_rho_aa, d_rho_bb, d_rho_ag, d_rho_bg, d_rho_ba]
+    # return [d_rho_gg, d_rho_aa, d_rho_bb, d_rho_ag, d_rho_bg, d_rho_ba]
+    return [
+        np.real(d_rho_gg),
+        np.real(d_rho_aa),
+        np.real(d_rho_bb),
+        np.real(d_rho_ag),
+        np.imag(d_rho_ag),
+        np.real(d_rho_bg),
+        np.imag(d_rho_bg),
+        np.real(d_rho_ba),
+        np.imag(d_rho_ba)
+    ]
 
 def run_simulation(params, t_span, t_eval):
     """Runs the solver for a single set of parameters."""
-    y0 = [1.0, 0.0, 0.0, 0.0+0j, 0.0+0j, 0.0+0j]
+    # y0 = [1.0, 0.0, 0.0, 0.0+0j, 0.0+0j, 0.0+0j]
+    y0 = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     
+    # sol = solve_ivp(
+    #     fun=master_equations,
+    #     t_span=t_span,
+    #     y0=y0,
+    #     t_eval=t_eval,
+    #     method='BDF', # Useful for stiff AMO sims where there are large time scale differences
+    #     args=(params,),
+    #     rtol=1e-6,
+    #     atol=1e-9
+    # )
     sol = solve_ivp(
         fun=master_equations,
         t_span=t_span,
         y0=y0,
         t_eval=t_eval,
-        method='BDF', # Useful for stiff AMO sims where there are large time scale differences
+        method='Radau',
         args=(params,),
         rtol=1e-6,
-        atol=1e-9
+        atol=1e-9,
+        first_step=1e-12
     )
     return sol
 
@@ -136,11 +167,6 @@ def plot_results(sol):
     
     plt.tight_layout()
     plt.show()
-    
-def calculate_stark(i, w):
-    # Energy must be divided by hbar to yield angular frequency
-    energy_joules = (i * con.e**2) / (2 * con.c * con.epsilon_0 * con.m_e * w**2)
-    return energy_joules / con.hbar
 
 def calculate_rabi2(i,w, crossSec, linWidth):
     return 0.5*np.sqrt((crossSec*linWidth*i)/(con.hbar*w))
@@ -195,108 +221,11 @@ def simulate_single_power(power, config, detuningSelect):
     
     return 0.0
 
-# --- Case 1: Power Scaling ---
-def run_power_scaling(base_params, w_2, crossPeak, linewidth405, t_int):
-    points = 20
-    powers = np.logspace(-1, 2, points) 
-    yields = []
-    bar_length = 20
-
-    t_span = (0, t_int)
-    t_eval = [t_int]
-    
-    beam_radius_m = 0.015 / 2.0
-    beam_area_m2 = np.pi * (beam_radius_m)**2
-    
-    # Pre-calculated 405 nm polarizability for the 5s^2 1S0 ground state
-    # From Safronova, M. S., et al. "Blackbody-radiation shift in the Sr optical atomic clock." Physical Review A 87.012509 (2013)
-    alpha_g_si = -1.0894e-38
-    
-    # The non-resonant background polarizability of the 5s5p 1P1 state. 
-    # The resonant Fano interaction is handled by the master equations.
-    alpha_a_nonres_si = 0.0 
-
-    for i, power in enumerate(powers):
-        intensity = power / beam_area_m2
-        
-        current_params = base_params.copy()
-        
-        # Ground state and intermediate state detunings driven by bound-state polarizabilities
-        shift_g = calculate_polarizability_shift(intensity, alpha_g_si)
-        shift_a = calculate_polarizability_shift(intensity, alpha_a_nonres_si)
-        current_params['del_ag'] = shift_a - shift_g
-        
-        # Autoionizing state detuning driven by the free-electron ponderomotive shift
-        shift_b = calculate_ponderomotive_shift(intensity, w_2)
-        current_params['del_ba'] = shift_b - shift_a
-        
-        # Total two-photon detuning
-        current_params['del_bg'] = current_params['del_ag'] + current_params['del_ba']
-        
-        current_params['rabi_2'] = calculate_rabi2(intensity, w_2, crossPeak, linewidth405)
-
-        sol = run_simulation(current_params, t_span, t_eval)
-        
-        if sol.success:
-            pop_gg = np.real(sol.y[0, -1])
-            pop_aa = np.real(sol.y[1, -1])
-            pop_bb = np.real(sol.y[2, -1])
-            yield_ion = 1.0 - (pop_gg + pop_aa + pop_bb)
-            yields.append(yield_ion)
-        else:
-            yields.append(0.0)
-
-        progress = (i + 1) / points
-        filled = int(bar_length * progress)
-        bar = '█' * filled + '-' * (bar_length - filled)
-        percent = int(progress * 100)
-        
-        print(f"\r[{bar}] {percent}% | Step {i + 1}/{points} | P: {power:6.2f} W", end='', flush=True)
-
-    print("\n--- Simulation Complete ---")
-    fig, ax1 = plt.subplots(figsize=(10, 6))
-    ax1.plot(powers, yields, marker='o', linestyle='-', color='blue')
-    ax1.set_title("Ionization Yield vs. Autoionization Laser Power")
-    ax1.set_xlabel("Laser Power (W)")
-    ax1.set_ylabel("Ion Yield")
-    ax1.grid(True, which="both", ls="--")
-    plt.tight_layout()
-    plt.show()
-
-# --- Case 2: Power Scaling but in Parallel ---
+# --- Case 1: Power Scaling of Ionization Efficiency ---
 # No isotope shift just a single atom
-def run_power_scaling_parallel(base_params, w_2, crossPeak, linewidth405, t_int, plotVal):
+def run_power_scaling_parallel(config, plotVal, finalEff):
     points = 20
     powers = np.logspace(-1, 2, points) 
-    yields = []
-    bar_length = 20
-
-    t_span = (0, t_int)
-    t_eval = [t_int]
-    
-    beam_radius_m = 0.015 / 2.0
-    beam_area_m2 = np.pi * (beam_radius_m)**2
-    
-    # Pre-calculated 405 nm polarizability for the 5s^2 1S0 ground state
-    # From Safronova, M. S., et al. "Blackbody-radiation shift in the Sr optical atomic clock." Physical Review A 87.012509 (2013)
-    alpha_g_si = -1.0894e-38
-    
-    # The non-resonant background polarizability of the 5s5p 1P1 state. 
-    # The resonant Fano interaction is handled by the master equations.
-    alpha_a_nonres_si = 0.0 
-
-    config = {
-    'beam_area_m2': beam_area_m2,
-    'base_params': base_params,
-    'alpha_g_si': alpha_g_si,
-    'alpha_a_nonres_si': alpha_a_nonres_si,
-    'w_2': w_2,
-    'crossPeak': crossPeak,
-    'linewidth405': linewidth405,
-    't_span': t_span,
-    't_eval': t_eval
-    }
-
     yields = Parallel(n_jobs=-1, verbose=10)(delayed(simulate_single_power)(power,config,0) for power in powers)
     
     if plotVal:
@@ -310,83 +239,85 @@ def run_power_scaling_parallel(base_params, w_2, crossPeak, linewidth405, t_int,
         plt.tight_layout()
         plt.show()
     
+    if finalEff:
+        print(f'Ionization probability of single Sr atom at 100W: {yields[-1]:.6f}')
+
     return powers, np.array(yields)
 
-# --- Case 3: Isotope Enrichment ---
-def run_iso_enrichment(parameters, w_1, w_2, P1, P2, crossPeak, isotopeList):
-    einstein461 = 2.01e8
-    linewidth405 = 2 * np.pi * 45 * con.c * 100
-    
-    # Extract Sr-84 shift dynamically to lock the laser frequency
-    shift_sr84 = next(iso["shift"] for iso in isotopeList if iso["name"] == "Sr-84")
-    w_laser = w_1 + 2 * np.pi * shift_sr84
-    
+def run_single_iso(config, iso):
+    isoConfig = config.copy()  
     Natom = 1e14
+    intensity1 = isoConfig['P1']/isoConfig['beam_area_m2'] 
     
-    beam_radius_m = 0.015 / 2.0
-    beam_area_m2 = np.pi * (beam_radius_m)**2
-    intensity1 = P1 / beam_area_m2    
+    w_1Iso = isoConfig['w_1'] + 2 * np.pi * iso["shift"]
     
-    alpha_g_si = -1.0894e-38
-    alpha_a_nonres_si = 0.0 
+    # Detuning between the Sr-84 locked laser and the evaluated isotope
+    detuningSelect = isoConfig['w_laser'] - w_1Iso
     
-    ions_produced = []
+    mu461 = np.sqrt((3 * con.pi * con.epsilon_0 * con.hbar * isoConfig['Einstein1'] * con.c**3) / (w_1Iso**3))
+    rabi461 = (mu461 / con.hbar) * np.sqrt((2 * intensity1) / (con.c * con.epsilon_0))
+    
+    iso_params = isoConfig['base_params'].copy()
+    iso_params['rabi_1'] = rabi461
+    
+    config['base_params'] = iso_params
 
-    for iso in isotopeList:
-        # Absolute transition frequency of the evaluated isotope
-        w_1Iso = w_1 + 2 * np.pi * iso["shift"]
-        
-        # Detuning between the Sr-84 locked laser and the evaluated isotope
-        detuningSelect = w_laser - w_1Iso
-        
-        mu461 = np.sqrt((3 * con.pi * con.epsilon_0 * con.hbar * einstein461 * con.c**3) / (w_1Iso**3))
-        rabi461 = (mu461 / con.hbar) * np.sqrt((2 * intensity1) / (con.c * con.epsilon_0))
-        
-        iso_params = parameters.copy()
-        iso_params['rabi_1'] = rabi461
-        
-        v_avg, t_int = calculate_interaction_time(T_celsius=530.0, L_cm=1.5, mass_amu=iso["mass"])
-        
-        config = {
-            'beam_area_m2': beam_area_m2,
-            'base_params': iso_params,
-            'alpha_g_si': alpha_g_si,
-            'alpha_a_nonres_si': alpha_a_nonres_si,
-            'w_2': w_2,
-            'crossPeak': crossPeak,
-            'linewidth405': linewidth405,
-            't_span': (0, t_int),
-            't_eval': [t_int]
-        }
-        
-        yield_ion = simulate_single_power(P2, config, detuningSelect)
-        iso_ion_count = Natom * iso["abundance"] * yield_ion
-        ions_produced.append(iso_ion_count)
-        
-    return ions_produced
-        
+    v_avg, t_int = calculate_interaction_time(T_celsius=530.0, L_cm=1.5, mass_amu=iso["mass"])
+
+
+    yield_ion = simulate_single_power(isoConfig['P2'], config, detuningSelect)
+    return yield_ion
 
 
 def main():
     # Experimental Parameters
+    # --- Beam Shape ---
+    beam_radius_m = 0.015 / 2.0
+    beam_area_m2 = np.pi * (beam_radius_m)**2 # 1.5cm diameter spot size
+
+    # --- Laser 1 ---
     lambda_1 = 460.73330e-9 # From https://physics.nist.gov/PhysRefData/ASD/lines_form.html (Sr-I)
     einstein461 = 2.01e8 # From NIST, [s^-1]
     w_1 = 2*con.pi*con.c/lambda_1
-    I1 = 565.884 # 0.1W with 1.5cm spot size
+    P1 = 0.1 # 0.1 W
+    I1 = P1/beam_area_m2 # 0.1W with 1.5cm spot size
     mu461 = np.sqrt((3*con.pi*con.epsilon_0*con.hbar*einstein461*con.c**3)/(w_1**3))
     rabi461 = (mu461/con.hbar)*np.sqrt((2*I1)/(con.c*con.epsilon_0))
     
-    
+    # --- Laser 2 ---
     lambda_2 = 405.16e-9 # From https://physics.nist.gov/PhysRefData/ASD/lines_form.html (Sr-I)
     linewidth405 = 2*con.pi*45*con.c*100
     w_2 = 2*con.pi*con.c/lambda_2
-    I2 = 5658.84 # 1W with 1.5cm diameter spot size
+    P2 = 1 # 1W
+    I2 = P2/beam_area_m2 # 1W with 
     crossPeak = 5.6e-19 # Peak cross section in m^2
     rabi405 = calculate_rabi2(I2,w_2,crossPeak,linewidth405)
 
+    # --- Isotope Data ---
+    isotopes = [
+        {"mass": 83.913419, "shift": -270.8e6, "abundance": 0.0056, "name": "Sr-84"},
+        {"mass": 85.90926073, "shift": -124.8e6, "abundance": 0.0986, "name": "Sr-86"},
+        {"mass": 86.90887750, "shift": -68.9e6, "abundance": 0.0700, "name": "Sr-87"},
+        {"mass": 87.90561226, "shift": 0.0, "abundance": 0.8258, "name": "Sr-88"},
+    ]
+    shift_sr84 = next(iso["shift"] for iso in isotopes if iso["name"] == "Sr-84") # Used in Selectivity Calc, where Sr84 is the target
+    w_laser = w_1 + 2 * np.pi * shift_sr84
+
+    # --- Atom Polarizability ---
+    # Pre-calculated 405 nm polarizability for the 5s^2 1S0 ground state
+    # From Safronova, M. S., et al. "Blackbody-radiation shift in the Sr optical atomic clock." Physical Review A 87.012509 (2013)
+    alpha_g_si = -1.0894e-38
+    
+    # The non-resonant background polarizability of the 5s5p 1P1 state. 
+    # The resonant Fano interaction is handled by the master equations.
+    alpha_a_nonres_si = 0.0 
+
     v_avg, t_int = calculate_interaction_time(T_celsius=530.0, L_cm=1.5, mass_amu=87.62)
 
+    t_span = (0, t_int)
+    t_eval = [t_int]
 
+    # --- Parameters for OBE Simulation ---
     base_params = {
         'rabi_1': rabi461,
         'rabi_2': rabi405,
@@ -398,22 +329,38 @@ def main():
         'del_ba': 0.0,
     }
 
+    # --- Experiment Config ---
+    config = {
+        'beam_area_m2': beam_area_m2,
+        'base_params': base_params,
+        'alpha_g_si': alpha_g_si,
+        'alpha_a_nonres_si': alpha_a_nonres_si,
+        'w_laser':w_laser,
+        'w_1': w_1,
+        'w_2': w_2,
+        'P1': P1,
+        'P2': P2,
+        'Einstein1':einstein461,
+        'linewidth405': linewidth405,
+        'crossPeak': crossPeak,
+        't_span': t_span,
+        't_eval': t_eval
+        }
+    
     # run_time_dynamics(base_params, w_2, crossPeak, linewidth405)
     # run_power_scaling(base_params, w_2, crossPeak, linewidth405, t_int)
-    run_power_scaling_parallel(base_params, w_2, crossPeak, linewidth405, t_int, True)
+    run_power_scaling_parallel(config, True, True)
 
-    # Isotope Enrichment Calcs
-    isotopes = [
-        {"mass": 83.913419, "shift": -270.8e6, "abundance": 0.0056, "name": "Sr-84"},
-        {"mass": 85.90926073, "shift": -124.8e6, "abundance": 0.0986, "name": "Sr-86"},
-        {"mass": 86.90887750, "shift": -68.9e6, "abundance": 0.0700, "name": "Sr-87"},
-        {"mass": 87.90561226, "shift": 0.0, "abundance": 0.8258, "name": "Sr-88"},
-    ]
-    
-    # Center on Sr84
-    
+    isoConfig = config.copy()
+    isoConfig['P2'] = 100 # 100W testing for selectivity
 
-
+    # --- Unit Comparison with standard power scaling ---
+    # iso84 = next(iso for iso in isotopes if iso["name"] == "Sr-84")
+    # yield84 = run_single_iso(isoConfig,iso84)
+    # print(f'Ionization Probability of Sr84 when targeted: {yield84:.6f}')
+    # iso88 = next(iso for iso in isotopes if iso["name"] == "Sr-88")
+    # yield88 = run_single_iso(isoConfig,iso88)
+    # print(f'Ionization Probability of Sr88 when targeted: {yield88:.6f}')
 
 if __name__ == "__main__":
     main()
